903edf714e5b3e079ca894b4ebea2c6e437a5eeb
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-04-14 00:01:31 -0300

    Create Week0.py

---
+++ Week0-Functions.py
@@ -0,0 +1,42 @@
+# CS50's Introduction to Programming with Python (CS50P) 2022 
+# https://cs50.harvard.edu/python/2022/weeks/0/
+# https://www.youtube.com/playlist?list=PLhQjrBD2T3817j24-GogXmWqO5Q5vYy0V
+# https://youtu.be/JP7ITIXGpHk
+
+# Week 0 - Functions and Variables
+# Lecture 0
+
+"""
+
+Index
+  Creating your first programs in Python;
+  Functions;
+  Bugs;
+  Variables;
+  Comments;
+  Pseudocode;
+  Strings;
+  Parameters;
+  Formatted Strings;
+  Integers;
+  Principles of readability;
+  Floats;
+  Creating your own functions; and
+  Return values.
+
+Creating Code with Python
+VS Code is a special type of text editor that is called a compiler. At the top, you’ll notice a text editor and, at the bottom you will see a terminal where you can execute commands.
+In the terminal, you can execute code hello.py to start coding.
+In the text editor above, you can type print("hello, world"). This is a famous canonical program that nearly all coders write during their learning process.
+In the terminal window, you can execute commands. To run this program, you are going to need to move your cursor to the bottom of the screen, clicking in the terminal window. You can now type a second command in the terminal window. Next to the dollar sign, type python hello.py and press the enter key on your keyboard.
+Recall, computers really only understand zeros and ones. Therefore, when you run python hello.py, python will interpret the text that you created in hello.py and translate it into the zeros and ones that the computer can understand.
+The result of running the python hello.py program is hello, world.
+Congrats! You just created your first program.
+
+Functions
+Functions are verbs or actions that the computer or computer language will already know how to perform.
+In your hello.py program, the print function knows how to print to the terminal window.
+The print function takes arguments. In this case, "hello, world" are the arguments that the print function takes.
+
+
+"""
