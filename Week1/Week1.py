89d292b65f71678590fedec196dbd37e1d70e8a2
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-04-22 00:58:39 -0300

    Update Week1.py

--- Week1/Week1.py
+++ Week1/Week1.py
@@ -257,59 +257,62 @@ Notice that the program will evaluate what is happening within the n % 2 == 0 as
 match
 Similar to if, elif, and else statements, match statements can be used to conditionally run code that matches certain values.
 Consider the following program:
-  name = input("What's your name? ")
+    name = input("What's your name? ")
 
-  if name == "Harry":
-      print("Gryffindor")
-  elif name == "Hermione":
-      print("Gryffindor")
-  elif name == "Ron": 
-      print("Gryffindor")
-  elif name == "Draco":
-      print("Slytherin")
-  else:
-      print("Who?")
-Notice the first three conditional statements print the same response.
+    if name == "Harry":
+        print("Gryffindor")
+    elif name == "Hermione":
+        print("Gryffindor")
+    elif name == "Ron": 
+        print("Gryffindor")
+    elif name == "Draco":
+        print("Slytherin")
+    else:
+        print("Who?")
+    Notice the first three conditional statements print the same response.
 
 We can improve this code slightly with the use of the or keyword:
-  name = input("What's your name? ")
+    name = input("What's your name? ")
 
-  if name == "Harry" or name == "Hermione" or name == "Ron": 
-      print("Gryffindor")
-  elif name == "Draco":
-      print("Slytherin")
-  else:
-      print("Who?")
-Notice the number of elif statements has decreased, improving the readability of our code.
+    if name == "Harry" or name == "Hermione" or name == "Ron": 
+        print("Gryffindor")
+    elif name == "Draco":
+        print("Slytherin")
+    else:
+        print("Who?")
+    Notice the number of elif statements has decreased, improving the readability of our code.
 
 Alternatively, we can use match statements to map names to houses. Consider the following code:
-  name = input("What's your name? ")
+    name = input("What's your name? ")
 
-  match name: 
-      case "Harry":
-          print("Gryffindor")
-      case "Hermione":
-          print("Gryffindor")
-      case "Ron": 
-          print("Gryffindor")
-      case "Draco":
-          print("Slytherin")
-      case _:
-          print("Who?")
-Notice the use of the _ symbol in the last case. This will match with any input, resulting in similar behavior as an else statement.
+    match name: 
+        case "Harry":
+            print("Gryffindor")
+        case "Hermione":
+            print("Gryffindor")
+        case "Ron": 
+            print("Gryffindor")
+        case "Draco":
+            print("Slytherin")
+        case _:
+            print("Who?")
+    Notice the use of the _ symbol in the last case. This will match with any input, resulting in similar behavior as an else statement.
 
 A match statement compares the value following the match keyword with each of the values following the case keywords. In the event a match is found, the respective indented code section is executed and the program stops the matching.
 We can improve the code:
-  name = input("What's your name? ")
+    name = input("What's your name? ")
 
-  match name: 
-      case "Harry" | "Hermione" | "Ron":
-          print("Gryffindor")
-      case "Draco":
-          print("Slytherin")
-      case _:
-          print("Who?")
+    match name: 
+        case "Harry" | "Hermione" | "Ron":
+            print("Gryffindor")
+        case "Draco":
+            print("Slytherin")
+        case _:
+            print("Who?")
 Notice, the use of the single vertical bar |. Much like the or keyword, this allows us to check for multiple values in the same case statement.
+
+
+
 
 Summing Up
 You now have the power within Python to use conditional statements to ask questions and have your program take action accordingly. In this lecture, we discussed…
---
+++ Week1/house.py
@@ -0,0 +1,45 @@
+name = input("What's your name? ")
+
+if name == "Harry":
+    print("Gryffindor")
+elif name == "Hermione":
+    print("Gryffindor")
+elif name == "Ron":
+    print("Gryffindor")
+elif name == "Draco":
+    print("Slythering")
+else: 
+    print("Who!?")
+
+
+
+if name == "Harry" or name == "Hermione" or name == "Ron":
+    print("Gryffindor")
+elif name == "Draco":
+    print("Slythering")
+else: 
+    print("Who!?")
+
+
+
+match name: # samesame Switch-Case-Break from JavaScript
+    case "Harry":
+        print("Gryffindor")
+    case "Hermione":
+        print("Gryffindor")
+    case "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _: # _ same as default from Switch-Case from JS...
+    print("Who!?")
+
+
+
+match name: # samesame Switch-Case-Break from JavaScript
+    case "Harry" | "Hermione" | "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _: # _ same as default from Switch-Case from JS...
+    print("Who!?")
