1961c5b2f31716b78fc835faf194e7eab384e531
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-05-04 23:09:46 -0300

    Update Week2.py

--- Week2/Week2.py
+++ Week2/Week2.py
@@ -3,3 +3,17 @@
 # https://www.youtube.com/playlist?list=PLhQjrBD2T3817j24-GogXmWqO5Q5vYy0V
 # https://youtu.be/-7xg8pGcP6w
 
+# https://cs50.harvard.edu/python/2022/notes/2/
+
+"""
+Lecture 2
+Loops
+While Loops
+For Loops
+Improving with User Input
+More About Lists
+Length
+Dictionaries
+Mario
+Summing Up
+"""
