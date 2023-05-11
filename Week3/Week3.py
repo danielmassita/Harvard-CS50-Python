af683f8081768acb57127e353d23b7b0d1d5789f
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-05-11 00:39:43 -0300

    Update Week3.py

--- Week3/Week3.py
+++ Week3/Week3.py
@@ -4,3 +4,34 @@
 # https://youtu.be/LW7g1169v7w
 
 # https://cs50.harvard.edu/python/2022/notes/3/
+
+"""
+LECTURE 3
+
+Exceptions
+Runtime Errors
+try
+else
+Creating a Function to Get an Integer
+pass
+Summing Up
+"""
+
+
+
+"""
+Exceptions
+Exceptions are things that go wrong within our coding.
+In our text editor, type code hello.py to create a new file. Type as follows (with the intentional errors included):
+
+print("hello, world)
+Notice that we intentionally left out a quotation mark.
+
+Running python hello.py in our terminal window, an error is outputted. The compiler states that it is a “syntax error.”” Syntax errors are those that require you to double-check that you typed in your code correction.
+
+You can learn more in Python’s documentation of Errors and Exceptions.
+"""
+
+
+
+"""
