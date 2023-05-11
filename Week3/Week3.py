a0147cefa5613449568c53372f4ce93c5bf69505
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-05-11 00:31:22 -0300

    Create Week3.py

---
+++ Week3/Week3.py
@@ -0,0 +1,6 @@
+# CS50's Introduction to Programming with Python (CS50P) 2022 
+# https://cs50.harvard.edu/python/2022/weeks/3/
+# https://www.youtube.com/playlist?list=PLhQjrBD2T3817j24-GogXmWqO5Q5vYy0V
+# https://youtu.be/LW7g1169v7w
+
+# https://cs50.harvard.edu/python/2022/notes/3/
