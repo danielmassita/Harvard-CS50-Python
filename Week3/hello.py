8607eed6c42dedf585c440083d2261ef5d2ed7f6
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-05-20 23:10:03 -0300

    Update hello.py

--- Week3/hello.py
+++ Week3/hello.py
@@ -1,3 +1,11 @@
-print("hello world)
-      pass
-      
+>>> print("hello world")
+hello world
+
+# SYNTAX ERROR
+
+>>> print("hello world)
+  File "<stdin>", line 1
+    print("hello world)
+          ^
+SyntaxError: unterminated string literal (detected at line 1)
+
