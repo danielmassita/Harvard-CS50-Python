b0e6cf0d7ac69a7483798f5f68805f267a475b4e
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-05-20 23:25:28 -0300

    Update number.py

--- Week3/number.py
+++ Week3/number.py
@@ -19,11 +19,35 @@ ValueError: invalid literal for int() with base 10: 'cat'
 
 
 
-# TRY
+# TRY - here we catch the error ValueError and return a message saying it is not an integer when we type 'cat'. 
 
 try: 
     x = int(input("What's x? "))
     print(f"The value of x is {x}.")
 except ValueError:
     print("We have a ValueError, for x is not an INTeger.")
+
+try:
+    x = int(input("What's x?"))
+except ValueError:
+    print("x is not an integer")
+
+print(f"x is {x}") # The idea is to reduce "try" to minimun lines of code, so we keep the output outside of the scope...
+
+# NAME ERROR - So if we take out the print() we should receive the message of the except catched by the ValueError...
+
+try:
+    x = int(input("What's x?"))
+except ValueError:
+    print("x is not an integer")
+
+print(f"x is {x}")
+
+# We find ourselves in a new NameError: x is not defined. 
+
+"""
+X is not defined, even thou we have written it. The idea behind it is that when we prompt the user for a string (input) converted to an int int() we should expect a number.
+When the user types something different of a number, hits enter, or put 'cat' into the prompt, the syntax will be truked and an error will occur "ValueError" and as an exception situation, the function WILL NOT ASSIGN TO X anything.
+Without an assignment to X of an integer, X is not defined, creating another problem of NameError. 
+"""
 
