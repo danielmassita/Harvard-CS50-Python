1a58e671e22e0adcf3ce40fd5360a5206461cd9d
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-05-20 23:13:23 -0300

    Update number.py

---
+++ Week3/number.py
@@ -0,0 +1,16 @@
+x = int(input("What's x? "))
+
+print(f"The value of x is {x}.")
+
+# VALUE ERROR
+
+>>> x = int(input("What's x? "))
+What's x? 10
+
+>>> x = int(input("What's x? "))
+What's x? cat
+
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+ValueError: invalid literal for int() with base 10: 'cat'
+
