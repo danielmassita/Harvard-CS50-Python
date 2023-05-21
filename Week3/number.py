0eb7a980949c521a16a481e1abb8b0ba2b7826f3
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-05-20 23:16:08 -0300

    Update number.py

--- Week3/number.py
+++ Week3/number.py
@@ -1,9 +1,11 @@
 x = int(input("What's x? "))
-
 print(f"The value of x is {x}.")
+
+
 
 # VALUE ERROR
 
+"""
 >>> x = int(input("What's x? "))
 What's x? 10
 
@@ -13,4 +15,15 @@ What's x? cat
 Traceback (most recent call last):
   File "<stdin>", line 1, in <module>
 ValueError: invalid literal for int() with base 10: 'cat'
+"""
+
+
+
+# TRY
+
+try: 
+    x = int(input("What's x? "))
+    print(f"The value of x is {x}.")
+except ValueError:
+    print("We have a ValueError, for x is not an INTeger.")
 
