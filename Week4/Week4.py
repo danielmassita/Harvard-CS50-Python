660ee986cfaf0970f7f249990e4eef3e812a9acf
danielmassita <daniel.massita@gmail.com>
2023-05-23 00:36:48 -0300

    Update Week4

--- Week4/Week4.py
+++ Week4/Week4.py
@@ -75,5 +75,15 @@ You can learn more in Python’s documentation of random.
 
 
 """
+Statistics
+Python comes with a built-in statistics library. How might we use this module?
+average is a function of this library that is quite useful. In your terminal window, type code average.py. In the text editor window, modify your code as follows:
 
+import statistics
+
+print(statistics.mean([100, 90]))
+Notice that we imported a different library called statistics. The mean function takes a list of values. This will print the average of these values. In your terminal window, type python average.py.
+
+Consider the possibilities of using the statistics module in your own programs.
+You can learn more in Python’s documentation of statistics.
 """
