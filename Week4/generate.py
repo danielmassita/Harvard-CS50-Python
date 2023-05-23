4a6edab9bd397ee2f1aae3f5f032ceed17d9e57d
danielmassita <daniel.massita@gmail.com>
2023-05-22 23:57:25 -0300

    Update Week4

--- Week4/generate.py
+++ Week4/generate.py
@@ -5,8 +5,23 @@ coin = random.choice(["heads", "tails"])
 print(coin)
 """
 
+"""
 from random import choice
 
 coin = choice(["heads", "tails"])
 print(coin)
+"""
+
+import random
+
+# random.randint(a, b)
+number = random.randint(1, 10)
+print(number)
+
+# random.shuffle(x)
+cards = []
+cards = ["jack", "queen", "king"]
+random.shuffle(cards)
+for card in cards:
+    print(card)
 
