6c3a279eda71f2c2f6269a162decb50240a13da8
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-05-26 11:44:38 -0300

    Update Week5

---
+++ Week5/Week5.py
@@ -0,0 +1,7 @@
+# CS50's Introduction to Programming with Python (CS50P) 2022 
+# https://cs50.harvard.edu/python/2022/weeks/5/
+# https://www.youtube.com/playlist?list=PLhQjrBD2T3817j24-GogXmWqO5Q5vYy0V
+# https://youtu.be/tIrcxwLqzjQ
+
+# https://cs50.harvard.edu/python/2022/notes/5/
+
