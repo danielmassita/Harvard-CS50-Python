492bfc2d85787aca0f8d348c41cfc9472557e91c
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-08-05 23:14:22 -0300

    Update Week5.py

--- Week5/Week5.py
+++ Week5/Week5.py
@@ -10,3 +10,35 @@
 print("Hello, world!")
 print("Hello, world!")
 print("Hello, world!")
+
+"""
+Lecture 5 - Unit Tests
+  assert
+  pytest
+  Testing Strings
+  Organizing Tests into Folders
+  Summing Up
+"""
+
+"""
+Unit Tests
+Up until now, you have been likely testing your own code using print statements.
+Alternatively, you may have been relying upon CS50 to test your code for you!
+It’s most common in industry to write code to test your own programs.
+In your console window, type code calculator.py. Note that you may have previously coded this file in a previous lecture. In the text editor, make sure that your code appears as follows:
+"""
+def main ():
+  x = int(input("What's x? "))
+  print("x squared is", square(x))
+
+
+def square(n):
+  return n * n
+
+
+if __name__ == "__main__":
+  main()
+"""
+Notice that you could plausibly test the above code on your own using some obvious numbers such as 2. However, consider why you might want to create a test that ensures that the above code functions appropriately.
+Following convention, let’s create a new test program by typing code test_calculator.py and modify your code in the text editor as follows:
+"""
