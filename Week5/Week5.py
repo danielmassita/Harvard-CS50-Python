827fbe7b7d1f4179eb622e39a1e27a50dad38e4e
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-08-06 22:46:21 -0300

    Update Week5.py

--- Week5/Week5.py
+++ Week5/Week5.py
@@ -42,3 +42,24 @@ if __name__ == "__main__":
 Notice that you could plausibly test the above code on your own using some obvious numbers such as 2. However, consider why you might want to create a test that ensures that the above code functions appropriately.
 Following convention, let’s create a new test program by typing code test_calculator.py and modify your code in the text editor as follows:
 """
+from calculator import square
+
+
+def main():
+    test_square()
+
+
+def test_square():
+    if square(2) != 4:
+        print("2 squared was not 4")
+    if square(3) != 9:
+        print("3 squared was not 9")
+
+
+if __name__ == "__main__":
+    main()
+"""
+Notice that we are importing the square function from square.py on the first line of code. By convention, we are creating a function called test_square. Inside that function, we define some conditions to test.
+In the console window, type python test_calculator.py. You’ll notice that nothing is being outputted. It could be that everything is running fine! Alternatively, it could be that our test function did not discover one of the “corner cases” that could produce an error.
+Right now, our code tests two conditions. If we wanted to test many more conditions, our test code could easily become bloated. How could we expand our test capabilities without expanding our test code?
+"""
