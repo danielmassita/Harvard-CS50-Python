060c9deaf3e3f5183acbb499bcadbb754851f240
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-08-08 01:08:38 -0300

    Update Week5.py

--- Week5/Week5.py
+++ Week5/Week5.py
@@ -21,11 +21,12 @@ Lecture 5 - Unit Tests
 """
 
 """
-Unit Tests
-Up until now, you have been likely testing your own code using print statements.
-Alternatively, you may have been relying upon CS50 to test your code for you!
-It’s most common in industry to write code to test your own programs.
-In your console window, type code calculator.py. Note that you may have previously coded this file in a previous lecture. In the text editor, make sure that your code appears as follows:
+UNIT TESTS
+
+- Up until now, you have been likely testing your own code using print statements.
+- Alternatively, you may have been relying upon CS50 to test your code for you!
+- It’s most common in industry to write code to test your own programs.
+- In your console window, type code calculator.py. Note that you may have previously coded this file in a previous lecture. In the text editor, make sure that your code appears as follows:
 """
 def main ():
   x = int(input("What's x? "))
@@ -39,8 +40,8 @@ def square(n):
 if __name__ == "__main__":
   main()
 """
-Notice that you could plausibly test the above code on your own using some obvious numbers such as 2. However, consider why you might want to create a test that ensures that the above code functions appropriately.
-Following convention, let’s create a new test program by typing code test_calculator.py and modify your code in the text editor as follows:
+- Notice that you could plausibly test the above code on your own using some obvious numbers such as 2. However, consider why you might want to create a test that ensures that the above code functions appropriately.
+- Following convention, let’s create a new test program by typing code test_calculator.py and modify your code in the text editor as follows:
 """
 from calculator import square
 
@@ -59,12 +60,14 @@ def test_square():
 if __name__ == "__main__":
     main()
 """
-Notice that we are importing the square function from square.py on the first line of code. By convention, we are creating a function called test_square. Inside that function, we define some conditions to test.
-In the console window, type python test_calculator.py. You’ll notice that nothing is being outputted. It could be that everything is running fine! Alternatively, it could be that our test function did not discover one of the “corner cases” that could produce an error.
-Right now, our code tests two conditions. If we wanted to test many more conditions, our test code could easily become bloated. How could we expand our test capabilities without expanding our test code?
+- Notice that we are importing the square function from square.py on the first line of code. By convention, we are creating a function called test_square. Inside that function, we define some conditions to test.
+- In the console window, type python test_calculator.py. You’ll notice that nothing is being outputted. It could be that everything is running fine! Alternatively, it could be that our test function did not discover one of the “corner cases” that could produce an error.
+- Right now, our code tests two conditions. If we wanted to test many more conditions, our test code could easily become bloated. How could we expand our test capabilities without expanding our test code?
+- Luckly, I only see one error, because by chance, 2 squared with 2+2 indeed equals 4.
 
-assert
-Python’s assert command allows us to tell the compiler that something, some assertion, is true. We can apply this to our test code as follows:
+ASSERT
+
+- Python’s assert command allows us to tell the compiler that something, some assertion, is true. We can apply this to our test code as follows:
 """
 from calculator import square
 
