62230a70341d1266b17d1f64385b21bbcaf110fa
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-08-19 23:21:52 -0300

    Update Week7.py

--- Week7/Week7.py
+++ Week7/Week7.py
@@ -45,3 +45,36 @@ Lecture 7
     else:
         print("Invalid")
 
+# Notice how the strip method is used to determine if username exists and if . is inside the domain variable. Running this program, a standard email address typed in by you could be considered valid. Typing in malan@harvard alone, you’ll find that the program regards this input as invalid.
+# We can be even more precise, modifying our code as follows:
+    
+    email = input("What's your email? ").strip()
+    username, domain = email.split("@")
+    if username and domain.endswith(".edu"):
+        print("Valid")
+    else:
+        print("Invalid")
+
+# Notice how the endswith method will check to see if domain contains .edu. Still, however, a nefarious user could still break our code. For example, a user could type in malan@.edu and it would be considered valid.
+# Indeed, we could keep iterating upon this code ourselves. However, it turns out that Python has an existing library called re that has a number of built-in functions that can validate user inputs against patterns.
+# One of the most versatile functions within the library re is search.
+# The search library follows the signature re.search(pattern, string, flags=0). Following this signature, we can modify our code as follows:
+    
+    import re
+    email = input("What's your email? ").strip()
+    if re.search("@", email):
+        print("Valid")
+    else:
+        print("Invalid")
+
+# Notice this does not increase the functionality of our program at all. In fact, it is somewhat a step back.
+# We can further our program’s functionality. However, we need to advance our vocabulary around validation. It turns out that in the world of regular expressions there are certain symbols that allow us to identify patterns. At this point, we have only been checking for specific pieces of text like @. It so happens that many special symbols can be passed to the compiler for the purpose of engaging in validation. A non-exhaustive list of those patterns is as follows:
+"""
+    .   any character except a new line
+    *   0 or more repetitions
+    +   1 or more repetitions
+    ?   0 or 1 repetition
+    {m} m repetitions
+    {m,n} m-n repetitions
+"""
+
