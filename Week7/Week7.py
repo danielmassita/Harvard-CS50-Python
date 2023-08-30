39f2279258cb73b53104b70be01dec12caf2445b
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-08-29 23:39:59 -0300

    Update Week7.py

--- Week7/Week7.py
+++ Week7/Week7.py
@@ -272,17 +272,30 @@ else:
 # You should never expect your users to always follow your hopes for clean input. Indeed, users will often violate your intentions as a programmer.
 # There are ways to clean up your data.
 # In the terminal window, type code format.py. Then, in the text-editor, code as follows:
-
-name = input("What's your name? ").strip()
-print(f"hello, {name}")
+    
+    name = input("What's your name? ").strip()
+    print(f"hello, {name}")
 
 # Notice that we have created, essentially, a “hello world” program. Running this program and typing in David, it works well! However, typing in Malan, David notice how the program does not function as intended. How could we modify our program to clean up this input?
 # Modify your code as follows.
+    
+    name = input("What's your name? ").strip()
+    if "," in name:
+        last, first = name.split(", ")
+        name = f"{first} {last}"
+    print(f"hello, {name}")
 
-name = input("What's your name? ").strip()
-if "," in name:
-    last, first = name.split(", ")
-    name = f"{first} {last}"
-print(f"hello, {name}")
+# Notice how last, first = name.split(", ") is run if there is a , in the name. Then, the name is standardized as first and last. Running our code, typing in Malan, David, you can see how this program does clean up at least one scenario where a user types in something unexpected.
+# You might notice that typing in Malan,David with no space causes the compiler to throw an error. Since we now know some regular expression syntax, let’s apply that to our code:
+    
+    import re
+    name = input("What's your name? ").strip()
+    matches = re.search(r"^(.+), (.+)$", name)
+    if matches:
+        last, first = matches.groups()
+        name = f"{first} {last}"
+    print(f"hello, {name}")
 
-# 
+# Notice that re.search can return a set of matches that are extracted from the user’s input. If matches are returned by re.search. Running this program, typing in David Malan notice how the if condition is not run and the name is returned. If you run the program by typing Malan, David, the name is also returned properly.
+# It just so happens that we can request specific groups back using matches.group. We can modify our code as follows:
+
