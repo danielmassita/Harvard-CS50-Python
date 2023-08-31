a2d393d3cb73fc458d106ae4278c20af9925d892
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-08-30 23:55:48 -0300

    Update Week7.py

--- Week7/Week7.py
+++ Week7/Week7.py
@@ -299,3 +299,51 @@ else:
 # Notice that re.search can return a set of matches that are extracted from the user’s input. If matches are returned by re.search. Running this program, typing in David Malan notice how the if condition is not run and the name is returned. If you run the program by typing Malan, David, the name is also returned properly.
 # It just so happens that we can request specific groups back using matches.group. We can modify our code as follows:
 
+import re
+
+name = input("What's your name? ").strip()
+matches = re.search(r"^(.+), (.+)$", name)
+if matches:
+    last = matches.group(1)
+    first = matches.group(2)
+    name = f"{first} {last}"
+print(f"hello, {name}")
+
+# Notice how, in this implementation, group is not plural (there is no s).
+# Our code can be further tightened as follows:
+
+    import re
+    name = input("What's your name? ").strip()
+    matches = re.search(r"^(.+), (.+)$", name)
+    if matches:
+        name = matches.group(2) + " " + matches.group(1)
+    print(f"hello, {name}")
+
+# Notice how group(2) and group(1) are concatenated together with a space. The first group is that which is left of the comma. The second group is that which is right of the comma.
+# Recognize still that typing in Malan,David with no space will still break our code. Therefore, we can make the following modification:
+
+    import re
+    name = input("What's your name? ").strip()
+    matches = re.search(r"^(.+), *(.+)$", name)
+    if matches:
+        name = matches.group(2) + " " + matches.group(1)
+    print(f"hello, {name}")
+
+# Notice the addition of the * in our validation statement. This code will now accept and properly process Malan,David. Further, it will properly handle ` David,Malan with many spaces in front of David`.
+# It is very common to utilize re.search as we have in the previous examples, where matches is on a line of code after. However, we can combine these statements:
+
+    import re
+    name = input("What's your name? ").strip()
+    if matches := re.search(r"^(.+), *(.+)$", name):
+        name = matches.group(2) + " " + matches.group(1)
+    print(f"hello, {name}")
+
+# Notice how we combine two lines of our code. The walrus := operator assigns a value from right to left and allows us to ask a boolean question at the same time. Turn your head sideways and you’ll see why this is called a walrus operator.
+# You can learn more in Python’s documentation of re.
+# https://docs.python.org/3/library/re.html
+
+
+
+
+
+# 
