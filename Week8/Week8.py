6bb071ac5c86d1180b4481e99af47426474e9bf9
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-09-10 23:37:14 -0300

    Create Week8.py

---
+++ Week8/Week8.py
@@ -0,0 +1,21 @@
+# CS50's Introduction to Programming with Python (CS50P) 2022 
+# https://cs50.harvard.edu/python/2022/weeks/8/
+# https://www.youtube.com/playlist?list=PLhQjrBD2T3817j24-GogXmWqO5Q5vYy0V
+# https://youtu.be/e4fwY9ZsxPw
+
+# https://cs50.harvard.edu/python/2022/notes/8/
+
+"""
+Lecture 8
+    Object-Oriented Programming
+    Classes
+    raise
+    Decorators
+    Connecting to Previous Work in this Course
+    Class Methods
+    Static Methods
+    Inheritance
+    Inheritance and Exceptions
+    Operator Overloading
+    Summing Up
+"""
