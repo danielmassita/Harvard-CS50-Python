de6cdd40bacffca1b8d8fbb18f22d6ae47ea58e3
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-09-25 07:38:51 -0300

    Update Week8.py

--- Week8/Week8.py
+++ Week8/Week8.py
@@ -637,3 +637,72 @@ if __name__ == "__main__":
 
 
 """
+
+
+"""
+Connecting to Previous Work in this Course
+
+- While not explicitly stated in past portions of this course, you have been using classes and objects the whole way through.
+- If you dig into the documentation of int, you’ll see that it is a class with a constructor. It’s a blueprint for creating objects of type int. You can learn more in Python’s documentation of int.
+- https://docs.python.org/3/library/functions.html#int
+- Strings too are also a class. If you have used str.lower(), you were using a method that came within the str class. You can learn more in Python’s documentation of str.
+- https://docs.python.org/3/library/stdtypes.html#str
+- list is also a class. Looking at that documentation for list, you can see the methods that are contained therein, like list.append(). You can learn more in Python’s documentation of list.
+- https://docs.python.org/3/library/stdtypes.html#list
+- dict is also a class within Python. You can learn more in Python’s documentation of dict.
+- https://docs.python.org/3/library/stdtypes.html#dict
+- To see how you have been using classes all along, go to your console and type code type.py and then code as follows:
+
+- To see how you have been using classes all along, go to your console and type code type.py and then code as follows:
+    print(type(50))
+- Notice how by executing this code, it will display that the class of 50 is int.
+
+- We can also apply this to str as follows:
+    print(type("hello, world"))
+- Notice how executing this code will indicate this is of the class str.
+
+- We can also apply this to list as follows:
+    print(type([]))
+- Notice how executing this code will indicate this is of the class list.
+
+- We can also apply this to a list using the name of Python’s built-in list class as follows:
+    print(type(list()))
+- Notice how executing this code will indicate this is of the class list.
+
+- We can also apply this to dict as follows:
+    print(type({}))
+- Notice how executing this code will indicate this is of the class dict.
+
+- We can also apply this to a dict using the name of Python’s built in dict class as follows:
+    print(type(dict()))
+- Notice how executing this code will indicate this is of the class dict.
+"""
+
+print(type(50))
+print(type(50.3))
+# <class 'int'>
+# <class 'float'>
+print("")
+
+print(type("50"))
+print(type("hello, world!"))
+# <class 'str'>
+# <class 'str'>
+print("")
+
+print(type([50]))
+print(type(list()))
+# <class 'list'>
+# <class 'list'>
+print("")
+
+print(type((50, 40)))
+# <class 'tuple'>
+print("")
+
+print(type({}))
+print(type(dict()))
+# <class 'dict'>
+# <class 'dict'>
+print("")
+
