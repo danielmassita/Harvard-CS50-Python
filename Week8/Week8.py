476f157c88e5bea01a148bf91dea63f1d23e7df7
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-09-27 13:29:35 -0300

    Update Week8.py

--- Week8/Week8.py
+++ Week8/Week8.py
@@ -871,4 +871,100 @@ Static Methods
 - https://www.digitalocean.com/community/tutorials/python-static-method
 - https://www.geeksforgeeks.org/class-method-vs-static-method-python/
 """
+
+class Student:
+    def __init__(self, name, house):
+        if not name:
+            raise ValueError("Missing name")
+        self.name = name
+        self.house = house
+
+    ...
+
+
+class Professor:
+    def __init__(self, name, subject):
+        if not name:
+            raise ValueError("Missing name")
+        self.name = name
+        self.subject = subject
+
+    ...
+
+"""
+Inheritance
+
+- Inheritance is, perhaps, the most powerful feature of object-oriented programming.
+- It just so happens that you can create a class that “inherits” methods, variables, and attributes from another class.
+- In the terminal, execute code wizard.py. Code as follows:
+"""
+class Wizard:
+    def __init__(self, name):
+        if not name:
+            raise ValueError("Missing name")
+        self.name = name
+
+    ...
+
+
+class Student(Wizard):
+    def __init__(self, name, house):
+        # the super() class is gonna be the PARENT class and INHERIT the class Wizard and all the molds, in the case, Wizard
+        super().__init__(name)
+        self.house = house
+
+    ...
+
+
+class Professor(Wizard):
+    def __init__(self, name, subject):
+        # the super() class is gonna be the PARENT class and INHERIT the class Wizard and all the molds, in the case, Wizard
+        super().__init__(name)
+        self.subject = subject
+
+    ...
+
+
+wizard = Wizard("Albus")
+student = Student("Harry", "Gryffindor")
+professor = Professor("Severus", "Defense Against the Dark Arts")
+
+print(wizard) # <__main__.Wizard object at 0x7f26be4c9790>
+print("")
+print(student) # <__main__.Student object at 0x7f26be4c97d0>
+print("")
+print(professor) # <__main__.Professor object at 0x7f26be4c9850>
+
+"""
+- Notice that there is a class above called Wizard and a class called Student. Further, notice that there is a class called Professor. Both students and professors have names. Also, both students and professors are wizards. Therefore, both Student and Professor inherit the characteristics of Wizard. Within the “child” class Student, Student can inherit from the “parent” or “super” class Wizard as the line super().__init__(name) runs the init method of Wizard. Finally, notice that the last lines of this code create a wizard called Albus, a student called Harry, and so on.
+
+
+
+Inheritance and Exceptions
+
+- While we have just introduced inheritance, we have been using this all along during our use of exceptions.
+- It just so happens that exceptions come in a heirarchy, where there are children, parent, and grandparent classes. These are illustrated below:
+
+
+    BaseException
+    +-- KeyboardInterrupt
+    +-- Exception
+        +-- ArithmeticError
+        |    +-- ZeroDivisionError
+        +-- AssertionError
+        +-- AttributeError
+        +-- EOFError
+        +-- ImportError
+        |    +-- ModuleNotFoundError
+        +-- LookupError
+        |    +-- KeyError
+        +-- NameError
+        +-- SyntaxError
+        |    +-- IndentationError
+        +-- ValueError
+    ...
+
+
+- You can learn more in Python’s documentation of exceptions.
+- https://docs.python.org/3/library/exceptions.html
 """
