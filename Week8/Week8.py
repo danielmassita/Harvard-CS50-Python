de5a4747a6e0ab90dd9e9b9edff9f793d7a7d51a
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-09-27 14:02:45 -0300

    Update Week8.py

--- Week8/Week8.py
+++ Week8/Week8.py
@@ -1039,30 +1039,30 @@ We can use OPERATOR OVERLOADING for the + signal
 
 """
 
-# class Vault():
-#     def __init__(self, galleons=0, sickles=0, knuts=0):
-#         self.galleons = galleons
-#         self.sickles = sickles
-#         self.knuts = knuts
+class Vault():
+    def __init__(self, galleons=0, sickles=0, knuts=0):
+        self.galleons = galleons
+        self.sickles = sickles
+        self.knuts = knuts
 
-#     def __str__(self):
-#         return f"{self.galleons} Galleons, {self.sickles} Sickles, {self.knuts} Knuts"
+    def __str__(self):
+        return f"{self.galleons} Galleons, {self.sickles} Sickles, {self.knuts} Knuts"
 
-#     def __add__(self, other):
-#         ...
+    def __add__(self, other):
+        ...
 
-# potter = Vault(100, 50, 25)
-# print(potter)
+potter = Vault(100, 50, 25)
+print(potter)
 
-# weasley = Vault(25, 50, 100)
-# print(weasley)
+weasley = Vault(25, 50, 100)
+print(weasley)
 
-# # galleons = potter.galleons + weasley.galleons
-# # sickles = potter.sickles + weasley.sickles
-# # knuts = potter.knuts + weasley.knuts
+# galleons = potter.galleons + weasley.galleons
+# sickles = potter.sickles + weasley.sickles
+# knuts = potter.knuts + weasley.knuts
 
-# total = potter + weasley
-# print(total)
+total = potter + weasley
+print(total)
 
 """
 But let's add this third.
