2924bcc9a6f8ce244af4d1e0472dad2e12ae8b07
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-09-27 13:11:18 -0300

    Update Week8.py

--- Week8/Week8.py
+++ Week8/Week8.py
@@ -835,4 +835,9 @@ Static Methods
 
 - It turns out that besides @classmethods, which are distinct from instance methods, there are other types of methods as well.
 - Using @staticmethod may be something you might wish to explore. While not covered explicitly in this course, you are welcome to go and learn more about static methods and their distinction from class methods.
+
+
+- https://www.digitalocean.com/community/tutorials/python-static-method
+- https://www.geeksforgeeks.org/class-method-vs-static-method-python/
+"""
 """
