2e45769d113502f378b156b06e00d6a996067203
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-10-21 23:36:35 -0300

    Create Week9.py

---
+++ Week9/Week9.py
@@ -0,0 +1,27 @@
+# CS50's Introduction to Programming with Python (CS50P) 2022 
+# https://cs50.harvard.edu/python/2022/weeks/9/
+# https://www.youtube.com/playlist?list=PLhQjrBD2T3817j24-GogXmWqO5Q5vYy0V
+# https://youtu.be/6pgodt1mezg?si=E7GfuAgLEX5p1Tmv
+
+# https://cs50.harvard.edu/python/2022/notes/9/
+
+"""
+Lecture 9
+  Et Cetera
+    set
+    Global Variables
+    Constants
+    Type Hints
+    Docstrings
+    argparse
+    Unpacking
+    args and kwargs
+    map
+    List Comprehensions
+    filter
+    Dictionary Comprehensions
+    enumerate
+    Generators and Iterators
+    Congratulations!
+    This was CS50!
+"""
